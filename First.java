98497376a1a061bf5d7facda40e1f9ad48410078
Genomic Invader <anikangel664@gmail.com>
2021-07-30 19:49:10 +0600

    The file is updated with new comments!

--- First.java
+++ First.java
@@ -1,9 +1,6 @@
 import java.lang.*;
-
 class First{
-
-public static void main(String args [])
-{
-System.out.println("Hello World");
-}
+    public static void main(String[] args) {
+        System.out.println("This is my First Code in Java! \nHurray!!!");
+    }
 }
